7040ffbae79afe132a0aafb6948e3e153f2ead78
Shreyash2330 <145776217+Shreyash2330@users.noreply.github.com>
2024-06-05 11:52:59 +0530

    main code running from app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from apps.utils.scrape import scrape_data 
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    print("Starting the application...")
+    scrape_data()
     app.run(debug=True)
