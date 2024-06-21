f251a71596244bf352e57b2cb65222a1cbb76d44
maxcode7055 <130527876+maxcode7055@users.noreply.github.com>
2024-06-21 14:57:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -137,3 +137,18 @@
 # article.nlp()
 # print("Keywords:", article.keywords)
 # print("Summary:", article.summary)
+
+
+
+
+
+import os
+from dotenv import load_dotenv
+from deep_translator import ChatGptTranslator
+
+text = """
+Yanna Krupnikov probes the motivations of Americans who avoid politics — but often vote. ezra kleinFrom New York Times Opinion, this is “The Ezra Klein Show.” If you are listening to this show, you’re an odd duck. I mean, I’m an odd duck, too. But if you’re here, you can probably, say, list the Trump trials off the top of your head. You can maybe quote inflation data going back months. You probably hector your friends about what’s in the I.R.A. And hell, you probably know what I.R.A. means. I mean, what’s wrong with you? We talk a lot about the left-right divide in politics, but there’s this other divide — interested and uninterested, the people who follow politics closely and the people who avoid it as much as they can. And I think that divide is bigger, or it’s at least harder to cross. If you’re a liberal who loves MSNBC, you kind of get a conservative who loves Fox News. You have different ideas and different views. The things that are attractive to them might be repellent to you, and vice versa. But you have a similar relationship to politics and political media. But if you’re the kind of person who can’t even imagine what it would be like to not know who the Speaker of the House is, it’s hard to imagine the media habits and political thinking of someone, then, who has negative interest in Mike Johnson. But people who don’t really follow politics do vote. In 2016, about 65 percent of them said they cast a vote for president. And Trump is winning this group handily right now. There was an NBC news poll from a few months ago that found 15 percent of voters don’t follow political news, but Trump was winning them by 26 points. When you go up the scale of interest, Biden does better. Down the scale, Trump does better. Biden needs to win some of these voters back. But what drives their votes, and how do you reach them when they actively dislike and avoid political media? Yanna Krupnikov is a professor of communication and media at the University of Michigan. Along with John Barry Ryan, she is the author of “The Other Divide: Polarization and Disengagement in American Politics.” So she literally wrote the book on this. So what did she learn? As always, my email, mailto:ezrakleinshow@nytimes.com. 
+"""
+translated = ChatGptTranslator(api_key=os.getenv('OPEN_AI_KEY'), target='fa').translate(text=text)
+# print(text, "@")
+print(translated)
