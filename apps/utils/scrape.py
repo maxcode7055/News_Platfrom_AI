158270e2bff34ada34063c457432c4a9c9862a77
Shreyash2330 <145776217+Shreyash2330@users.noreply.github.com>
2024-06-21 14:42:25 +0530

    changes in scarping methods

--- apps/utils/scrape.py
+++ apps/utils/scrape.py
@@ -1,5 +1,5 @@
-from serpapi import GoogleSearch
 import os
+from serpapi import GoogleSearch
 from dotenv import load_dotenv
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from newspaper import Article
@@ -7,64 +7,83 @@ import re
 from bs4 import BeautifulSoup
 import dateparser
 from datetime import datetime
+import time
+import requests
+import random
+import logging
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 IMAGE_EXTENSIONS_REGEX = r'\.(jpg|jpeg|png|webp|avif)$'
 
-load_dotenv()
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0"
+]
 
 def format_date(month, day, year):
     return f"{month}/{day}/{year}" if month and day and year else ""
 
 def clean_text(text):
-    cleaned_text = re.sub(r'[\\→\n\"/]', '', text)
+    cleaned_text = re.sub(r'\[\\→\n\"/\]', '', text)
     return cleaned_text.strip()
 
 def remove_unwanted_tags(soup):
     unwanted_tags = ['ul', 'header', 'footer', 'nav', 'a', 'strong', 'span', 'i', 'script', 'style']
-
     for tag in unwanted_tags:
         for el in soup.find_all(tag):
             el.decompose()
-
     return soup
 
 def remove_unwanted_sentences_from_html(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     soup = remove_unwanted_tags(soup)
-    
     return str(soup)
 
-def fetch_article_content(url, snippet):
-    try:
-        article = Article(url)
-        article.download()
-        article.parse()
-        article_html = article.html
-        article_html = remove_unwanted_sentences_from_html(article_html)
-        article.set_html(article_html)
-        article.parse()
+def fetch_article_content(url, snippet, retries=3):
+    headers = {'User-Agent': random.choice(USER_AGENTS)}
+    for attempt in range(retries):
+        try:
+            article = Article(url)
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            article.download(input_html=response.text)
+            article.parse()            
+            article_html = article.html
+            article_html = remove_unwanted_sentences_from_html(article_html)
+            article.set_html(article_html)
+            article.parse()
 
-        authors = article.authors
-        publish_date = article.publish_date
-        top_image = article.top_image
+            authors = ', '.join(article.authors) 
+            publish_date = article.publish_date
+            top_image = article.top_image if re.search(IMAGE_EXTENSIONS_REGEX, article.top_image, re.IGNORECASE) else None
 
-        if top_image and re.search(IMAGE_EXTENSIONS_REGEX, top_image, re.IGNORECASE):
             full_content = article.text
-
+            
             if not full_content.startswith(snippet):
                 full_content = re.sub(r'^.*?(?:\b%s\b\s*)' % re.escape(snippet), '', full_content, flags=re.IGNORECASE)
-                full_content = snippet + " " + full_content.strip()
-
+            full_content = snippet + " " + full_content.strip()
             full_content = clean_text(full_content)
             
-            print(f"Article Length: {len(full_content.split())} words")  
+            article_word_count = len(full_content.split())
+            logging.info(f"Article Length: {article_word_count} words")
+            
+            if article_word_count < 100:
+                logging.info(f"Skipping article: '{url}' - Content too short.")
+                return None, None, None, None
 
-            return full_content, authors, publish_date, top_image, None
-        else:
-            return None, None, None, None, "Invalid or missing top_image URL"
-
-    except Exception as e:
-        return None, None, None, None, f"Error fetching article content: {str(e)}"
+            return full_content, authors, publish_date, top_image
+        except requests.exceptions.RequestException as e:
+            logging.warning(f"Attempt {attempt + 1} failed for '{url}': {str(e)}")
+            time.sleep(2 ** attempt) 
+        except Exception as e:
+            logging.error(f"Error fetching article content from '{url}': {str(e)}")
+    logging.error(f"Failed to fetch article content from '{url}' after {retries} attempts.")
+    return None, None, None, None
 
 def convert_relative_date(date_str):
     if date_str:
@@ -86,11 +105,14 @@ def scrape_news(keyword, month, from_day, to_day, year):
             elif year:
                 tbs_param = f"cdr:1,cd_min:1/1/{year},cd_max:12/31/{year}"
 
+        start_from = 9 
+        
         params = {
             "q": keyword,
             "tbm": "nws",
             "engine": "google",
-            "num": "10",  
+            "start":start_from,
+            "num": "10",
             "tbs": tbs_param,
             "api_key": os.getenv('SERPAPI_KEY')
         }
@@ -98,39 +120,38 @@ def scrape_news(keyword, month, from_day, to_day, year):
         search = GoogleSearch(params)
         results = search.get_dict()
         news_results = results["news_results"]
+        logging.info("news_results: %s", news_results)
 
-        # Convert relative dates to absolute dates
         for news_item in news_results:
             if "date" in news_item:
                 news_item["date"] = convert_relative_date(news_item["date"])
 
         response_data = []
 
-        # Using ThreadPoolExecutor for parallel fetching
         with ThreadPoolExecutor(max_workers=5) as executor:
-            future_to_news_item = {executor.submit(fetch_article_content, result["link"], result.get("snippet", "")): result for result in news_results[:10]}
+            future_to_news_item = {executor.submit(fetch_article_content, result["link"], result.get("snippet", "")): result for result in news_results}
             for future in as_completed(future_to_news_item):
                 news_item = future_to_news_item[future]
-                full_content, authors, publish_date, top_image, error_message = future.result()
+                try:
+                    full_content, authors, publish_date, top_image = future.result()
+                    if full_content is not None and full_content != "":
+                        article_word_count = len(full_content.split())
+                        logging.info(f"Processing article: '{news_item['title']}' with {article_word_count} words")
 
-                if full_content is not None and full_content != "":
-                    article_word_count = len(full_content.split())
-                    print(f"Processing article: '{news_item['title']}' with {article_word_count} words")  # Debugging print statement
-                    # Check if full_content has less than 100 words
-                    if article_word_count >= 100:
                         news_item["full_content"] = full_content
                         news_item["authors"] = authors
                         news_item["publish_date"] = publish_date
                         news_item["top_image"] = top_image
                         response_data.append(news_item)
                     else:
-                        print(f"Skipping '{news_item['title']}': Full content has less than 100 words.")
-                else:
-                    # Optionally, you can include error handling or logging here
-                    print(f"Failed to fetch full content for '{news_item['title']}': {error_message}")
+                        logging.warning(f"Failed to fetch full content for '{news_item['title']}': No content available.")
+                except Exception as e:
+                    logging.error(f"Error processing article '{news_item['title']}': {str(e)}")
 
         return response_data
-
     except Exception as e:
+        logging.error(f"Error in scrape_news: {str(e)}")
         raise e
+
+
 
